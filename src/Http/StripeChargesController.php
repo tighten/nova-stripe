e9c0c4e4ed4edf88d67d62e7c203cba2618959d9
Samantha Geitz <samanthamichele7@gmail.com>
2018-08-29 13:39:28 -0500

    Cleanup the charges controller index method

--- src/Http/StripeChargesController.php
+++ src/Http/StripeChargesController.php
@@ -3,7 +3,6 @@
 namespace Tightenco\NovaStripe\Http;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Str;
 use Tightenco\NovaStripe\Clients\StripeClient;
 
 class StripeChargesController extends Controller
@@ -11,17 +10,7 @@ class StripeChargesController extends Controller
     public function index()
     {
         $charges = (new StripeClient)->listCharges(
-            collect([
-                'created' => request()->created,
-                'customer' => request()->customer,
-                'ending_before' => request()->customer,
-                'limit' => request()->limit,
-                'source' => request()->source,
-                'starting_after' => request()->starting_after,
-                'transfer_group' => request()->transfer_group
-            ])->reject(function($name) {
-                return empty($name);
-            })->toArray()
+            request()->only('created', 'customer', 'ending_before', 'limit', 'source', 'starting_after', 'transfer_group')
         );
 
         return response()->json(['charges' => $charges]);
