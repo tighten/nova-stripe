05c6ec8fc9bbe9cb76fa217b7d0d3233572851ce
faxblaster <alison.l.kirk@gmail.com>
2021-02-12 14:38:30 -0500

    Add Github Actions and initial tests

--- tests/StripeChargeControllerTest.php
+++ tests/StripeChargeControllerTest.php
@@ -3,13 +3,63 @@
 
 namespace Tightenco\NovaStripe\Tests;
 
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
+
 class StripeChargeControllerTest extends TestCase
 {
+    use WithFaker;
+
     /** @test */
     public function it_can_can_return_a_response()
     {
-        $this
-            ->get('nova-vendor/tightenco/nova-stripe/stripe/charges')
+        $this->get('nova-vendor/nova-stripe/stripe/charges')
             ->assertSuccessful();
+
+        $this->get('nova-vendor/nova-stripe/stripe/balance')
+            ->assertSuccessful();
+
+        $stripe = new \Stripe\StripeClient(Config::get('services.stripe.secret'));
+        $charge = $stripe->charges->create(
+            [
+            'amount' => $this->faker->numberBetween(50, 1000),
+            'currency' => 'usd',
+            'source' => 'tok_mastercard',
+            'description' => $this->faker->sentence,
+            ]
+        );
+
+        $this->get('nova-vendor/nova-stripe/stripe/charges/' . $charge->id)
+            ->assertSuccessful();
+    }
+
+    /** @test */
+    public function it_returns_a_list_of_charges()
+    {
+        $stripe = new \Stripe\StripeClient(Config::get('services.stripe.secret'));
+        $charge = $stripe->charges->create(
+            [
+            'amount' => $this->faker->numberBetween(50, 1000),
+            'currency' => 'usd',
+            'source' => 'tok_mastercard',
+            'description' => $this->faker->sentence,
+            ]
+        );
+
+        $this->get('nova-vendor/nova-stripe/stripe/charges')
+            ->assertJsonFragment(['description' => $charge->description])
+            ->assertJsonFragment(['amount' => $charge->amount]);
+    }
+
+    /** @test */
+    public function it_returns_charge_details()
+    {
+        $this->markTestSkipped();
+    }
+
+    /** @test */
+    public function it_shows_the_current_balance()
+    {
+        $this->markTestSkipped();
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tightenco\NovaStripe\Tests;
 
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Tightenco\NovaStripe\ToolServiceProvider;
@@ -12,7 +14,16 @@ abstract class TestCase extends Orchestra
     {
         parent::setUp();
 
+        Config::set('services.stripe.secret', getenv('STRIPE_SECRET'));
+
         Route::middlewareGroup('nova', []);
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->useEnvironmentPath(__DIR__ . '/..');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+        parent::getEnvironmentSetUp($app);
     }
 
     protected function getPackageProviders($app)
