d24a4a24ed2b7828d95055e9c1a4570fc5cbb7e0
faxblaster <alison.l.kirk@gmail.com>
2021-07-19 10:22:27 -0400

    Add refund icon

--- resources/js/components/ChargesTable.vue
+++ resources/js/components/ChargesTable.vue
@@ -36,7 +36,15 @@
 
                 <tbody v-for="charge in charges">
                 <tr>
-                    <td>{{ charge.id }}</td>
+                    <td>
+                        {{ charge.id }}
+                        <span v-if="charge.refunded" class="ml-3 text-70">
+                            <span class="sr-only">Refunded</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z" />
+                            </svg>
+                        </span>
+                    </td>
                     <td>{{ charge.currency | money(charge.amount) }}</td>
                     <td>{{ charge.created | date }}</td>
                     <td>{{ charge.status }}</td>
